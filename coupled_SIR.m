7f83dea9dad6fa799beac82cbdada6f16467f213
s-biber <ym18512@bristol.ac.uk>
2021-04-13 17:27:55 +0100

    optimisation routine

--- coupled_SIR.m
+++ coupled_SIR.m
@@ -2,31 +2,32 @@
 M=3; % number of countries
 gamma=1/14;
 %beta=(eye(M)*1.6 + 0.03)*gamma;
-beta=[1.6,0,0;
-      0.03,1.6,0;
-      0.03,0,1.6]*gamma;
+% beta=[1.6,0,0;
+%       0.03,1.6,0;
+%       0.03,0,1.6]*gamma;
 %beta=(eye(M)*1.6 + 0.03*2*rand(M))*gamma;
 
 %%
 %xi=[0.00;0.0;0.0];
 N=[6;3.6;3];
 %xi=[0.0;0.0028;0.0033]; %constaint: sum(xi)*N=0.02
-xi=@(t)[0;0;0];
+xi=@(t) [0.01;0.01;0.01];
 % S: X(1:M), I: X(M+1:2*M), R: X(2*M+1:3*M), V: X(3*M+1:4*M)
 odefun=@(t,X)[-beta*X(M+1:2*M).*X(1:M)./N - xi(t).*X(1:M)./(N-X(3*M+1:4*M)) ; 
                beta*X(M+1:2*M).*X(1:M)./N - gamma*X(M+1:2*M) - xi(t).*X(M+1:2*M)./(N-X(3*M+1:4*M)) ;
                gamma*X(M+1:2*M) - xi(t).*X(2*M+1:3*M)./(N-X(3*M+1:4*M)) ;
                xi(t)];
            
-tspan=[0,200];
+tspan=[0,500];
 Sinit=[0.9; 1; 1].*N;
 Iinit=[0.1; 0; 0].*N;
 Rinit=[0;0;0].*N;
 Vinit=[0;0;0].*N;
 init=[Sinit;Iinit;Rinit;Vinit];
 
+options = odeset('RelTol',1e-12,'AbsTol',1e-13,'Events',@(t,X) event_negative(t,X,M));
 
-[t,X]=ode45(odefun,tspan,init);
+[t,X]=ode45(odefun,tspan,init,options);
 
 %%
 fig=figure('Position',[121 346 1734 439]);
@@ -46,7 +47,7 @@ hold on
 plot(t,X(:,2)./N(2));
 plot(t,X(:,5)./N(2));
 plot(t,X(:,8)./N(2));
-plot(t,X(:,11)./N(1));
+plot(t,X(:,11)./N(2));
 xlabel('t');
 title('country 2');
 legend('S2','I2','R2','V2');
@@ -57,7 +58,7 @@ hold on
 plot(t,X(:,3)./N(3));
 plot(t,X(:,6)./N(3));
 plot(t,X(:,9)./N(3));
-plot(t,X(:,12)./N(1));
+plot(t,X(:,12)./N(3));
 xlabel('t');
 title('country 3');
 legend('S3','I3','R3','V3');
